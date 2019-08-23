34a3b305831ae68b6511e6f6dc96df1e2e931deb
Andrius Jurgsaitis <andriusj6@hotmail.com>
2019-08-23 06:30:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,16 @@
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    item.each do |key, value|
+      if new_hash[key]
+        new_hash[key][:count] += 1
+      else
+        new_hash[key] = value
+        new_hash[key][:count] = 1
+      end
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
