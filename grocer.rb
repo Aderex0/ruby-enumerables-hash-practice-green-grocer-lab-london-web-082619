c303486052db0313ac68f81901c70755df29fe51
Andrius Jurgsaitis <andriusj6@hotmail.com>
2019-08-23 07:57:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,7 +14,19 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  applied_cart = cart
+  coupons.each do |coupon|
+    coupon_item = coupon[:item]
+    if cart[coupon_item][:count] >= 2 && cart.keys.include?(coupon_item)
+      applied_cart[coupon_item + " W/COUPON"] = {
+        :price => coupon[:cost] / coupon[:num], #2.50
+        :clearance => cart[coupon_item][:clearance],
+        :count => coupon[:num]
+     }
+      applied_cart[coupon_item][:count] -= coupon[:num]
+    end
+  end
+  applied_cart
 end
 
 def apply_clearance(cart)
